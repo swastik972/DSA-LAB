8011434f643d8d370c1ceacc9b66406e024e000b
swastik972 <rawatswastik30@gmail.com>
2026-03-26 23:48:37 +0545

    added to new file

--- Shorting/shell_sort.c
+++ Shorting/shell_sort.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int arr[100], n, i, j, temp;
+    int arr[100], n, i, j, gap, temp;
 
     printf("Enter number of elements: ");
     scanf("%d", &n);
@@ -11,13 +11,13 @@ int main() {
         scanf("%d", &arr[i]);
     }
 
-    for(i = 0; i < n - 1; i++) {
-        for(j = 0; j < n - i - 1; j++) {
-            if(arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+    for(gap = n / 2; gap > 0; gap /= 2) {
+        for(i = gap; i < n; i++) {
+            temp = arr[i];
+            for(j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                arr[j] = arr[j - gap];
             }
+            arr[j] = temp;
         }
     }
 
